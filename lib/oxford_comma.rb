14b5733ec8b4d3c98e2b8a7841b4faa81825235d
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 18:07:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.size > 2
+     last = array.pop
+     string = "#{array.join(", ")}, and #{last}"
+  else
+    return array.join(" and ")
+  end
 end
