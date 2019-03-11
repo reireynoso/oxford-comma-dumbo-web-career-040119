3c885660cca815aad8d2ecc1409b88e6ac3bc497
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 18:06:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   string = ""
   if array.size > 2
      last = array.pop
-     string = "#{array.join(", ")}and #{last}"
+     string = "#{array.join(", ")} and #{last}"
   else
     return array.join(" and ")
   end
