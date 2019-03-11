5efa3e796ab34c91330f946c501ba4392bce87ae
Reinald Reynoso <reireynoso@gmail.com>
2019-03-11 17:59:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size > 2 
      array.join(" , ")
-     array[array.size - 1]a
+     array[array.size - 1]join(" and ")
   else
     return array.join(" and ")
   end
